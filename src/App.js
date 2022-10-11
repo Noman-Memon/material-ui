463e2e35a97db63d9f9b0393b9c368a0ebeb5201
ABASITJAMALBROTHERS <noman.rauf@jamalbrothers.com.pk>
2022-10-11 18:10:59 +0500

    1st example material UI

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg'
+import './App.css'
+import { Button } from '@mui/material'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <div className="App">
+        <h1>React-MUI</h1>
+        <button> Click here</button>
+        <div>
+          <Button variant="contained" color="secondary">
+            Secondary
+          </Button>
+          <Button variant="contained" color="success">
+            Success
+          </Button>
+          <Button variant="outlined" color="error">
+            Error
+          </Button>
+        </div>
+      </div>
+    </>
+  )
 }
 
-export default App;
+export default App
