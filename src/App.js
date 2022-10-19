3729af4f6fa7a953951b19992f6d8e2795860e54
ABASITJAMALBROTHERS <noman.rauf@jamalbrothers.com.pk>
2022-10-19 19:03:12 +0500

    create DataGrid in materialui

--- src/App.js
+++ src/App.js
@@ -1,35 +1,11 @@
-// import logo from './logo.svg'
 import './App.css'
-import { Button } from '@mui/material'
-import React, { useState } from 'react'
+import DataTable from './Components/DataTable'
 
 function App() {
-  const [color, setcolor] = useState('primary')
-  function changeColor() {
-    setcolor('secondary')
-  }
   return (
     <>
-      <div className="App">
-        <h1>React-MUI</h1>
-        <button> Click here</button>
-        <div>
-          <Button variant="contained" color="secondary">
-            Secondary
-          </Button>
-          <Button
-            variant="contained"
-            color={color}
-            onClick={() => {
-              changeColor()
-            }}
-          >
-            Success
-          </Button>
-          <Button variant="outlined" color="error">
-            Error
-          </Button>
-        </div>
+      <div classNam>
+        <DataTable />
       </div>
     </>
   )
